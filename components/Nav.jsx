a54c89ee2f4d7724852b689f90b12aa6055406b8
killshotxd <hassanansari211@gmail.com>
2022-10-15 17:04:25 +0530

    Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">What's on your Mind?</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        <Link href="/auth/login">
+          <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+            Join Now
+          </a>
+        </Link>
+      </ul>
+    </nav>
   );
 }
