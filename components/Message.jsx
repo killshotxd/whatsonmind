593780607323e759f05bd45e0bf9a9250ebf754c
killshotxd <hassanansari211@gmail.com>
2022-10-17 11:12:26 +0530

    User posts

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,11 +1,11 @@
 export default function Message({ children, avatar, userName, description }) {
   return (
     <div className="bg-white p-8 border-b-2 rounded-lg">
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
         <img src={avatar} className="w-10 rounded-full" alt="avatar" />
         <h2>{userName}</h2>
       </div>
-      <div>
+      <div className="py-4">
         <p>{description}</p>
       </div>
       {children}
