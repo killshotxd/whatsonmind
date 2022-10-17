3a5bf879d6a23a403f22a6e67b4d42ae01fb2bf8
killshotxd <hassanansari211@gmail.com>
2022-10-17 12:35:50 +0530

    Comments working

--- pages/[slug].js
+++ pages/[slug].js
@@ -3,12 +3,57 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { auth, db } from "../utils/Firebase";
 import { toast } from "react-toastify";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
 
 export default function Details() {
   const router = useRouter();
   const routerData = router.query;
   const [message, setMessage] = useState("");
   const [allMessages, setAllMessages] = useState([]);
+
+  // Submitting messages
+  const submitMessage = async () => {
+    // Check if user is logged
+    if (!auth.currentUser) return router.push("/auth/Login");
+    if (!message) {
+      toast.error("Do not leave an empty message 🙄", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return;
+    }
+    const docRef = doc(db, "posts", routerData.id);
+    await updateDoc(docRef, {
+      comments: arrayUnion({
+        message,
+        avatar: auth.currentUser.photoURL,
+        userName: auth.currentUser.displayName,
+        time: Timestamp.now(),
+      }),
+    });
+    setMessage("");
+  };
+
+  //Get Comments
+  const getComments = async () => {
+    const docRef = doc(db, "posts", routerData.id);
+    const unsubscribe = onSnapshot(docRef, (snapshot) => {
+      setAllMessages(snapshot.data().comments);
+    });
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    getComments();
+  }, [router.isReady]);
 
   return (
     <div>
@@ -22,21 +67,32 @@ export default function Details() {
             placeholder="Send a message 😁"
             className="bg-gray-800 w-full p-2  text-white text-sm"
           />
-          <button className="bg-cyan-500 text-white py-2 px-4 text-sm">
+          <button
+            onClick={submitMessage}
+            className="bg-cyan-500 text-white py-2 px-4 text-sm"
+          >
             Submit
           </button>
         </div>
         <div className="py-6">
           <h2 className="font-bold">Comments</h2>
-          {/* {setAllMessages?.map(message => (
-            <div>
-                <div>
-                    <img src="" alt="" />
-                </div>
+          {allMessages?.map((message) => (
+            <div className="bg-white p-4 my-4 border-2" key={message.time}>
+              <div className="flex items-center gap-2 mb-4">
+                <img
+                  className="w-10 rounded-full"
+                  src={message.avatar}
+                  alt="avatar"
+                />
+                <h2>{message.userName}</h2>
+              </div>
+              <h2>{message.message}</h2>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+// 2h21m
