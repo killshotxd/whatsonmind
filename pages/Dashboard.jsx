228cb783c61b2b9c30d7887bf79a1dd6cfb304df
killshotxd <hassanansari211@gmail.com>
2022-10-19 14:41:59 +0530

    Update Dashboard.jsx

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -17,20 +17,24 @@ import Message from "../components/Message";
 import { BsTrash2Fill } from "react-icons/bs";
 import { AiFillEdit } from "react-icons/ai";
 import Link from "next/link";
+import { BounceLoader } from "react-spinners";
 
 export default function Dashboard() {
   const route = useRouter();
   const [user, loading] = useAuthState(auth);
   const [posts, setPosts] = useState([]);
+  const [loader, setLoader] = useState(false);
   // Checks
 
   const getData = async () => {
     if (loading) return;
     if (!user) return route.push("auth/Login");
+    setLoader(true);
     const collectionRef = collection(db, "posts");
     const q = query(collectionRef, where("user", "==", user.uid));
     const unsubscribe = onSnapshot(q, (snapshot) => {
       setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoader(false);
     });
     return unsubscribe;
   };
@@ -48,29 +52,36 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Your posts</h1>
-      <div>
-        {posts.map((post) => {
-          return (
-            <Message {...post} key={post.id}>
-              <div className="flex gap-4">
-                <button
-                  onClick={() => deletePost(post.id)}
-                  className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm"
-                >
-                  <BsTrash2Fill className="text-2xl" />
-                  Delete
-                </button>
-                <Link href={{ pathname: "/Post", query: post }}>
-                  <button className="text-teal-600 flex items-center justify-center gap-2 py-2 text-sm">
-                    <AiFillEdit className="text-2xl" />
-                    Edit
+      {!loader ? (
+        <div>
+          {posts.map((post) => {
+            return (
+              <Message {...post} key={post.id}>
+                <div className="flex gap-4">
+                  <button
+                    onClick={() => deletePost(post.id)}
+                    className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm"
+                  >
+                    <BsTrash2Fill className="text-2xl" />
+                    Delete
                   </button>
-                </Link>
-              </div>
-            </Message>
-          );
-        })}
-      </div>
+                  <Link href={{ pathname: "/Post", query: post }}>
+                    <button className="text-teal-600 flex items-center justify-center gap-2 py-2 text-sm">
+                      <AiFillEdit className="text-2xl" />
+                      Edit
+                    </button>
+                  </Link>
+                </div>
+              </Message>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center mx-auto h-80">
+          <BounceLoader color="#06B6D4" />
+        </div>
+      )}
+
       <button
         className="font-medium text-white bg-gray-800 py-2 px-4 my-6"
         onClick={() => auth.signOut()}
